70d1f8338cefd9d7979472ab48d397c720b48c86
Dmitrii <62088955+Dikin01@users.noreply.github.com>
2022-09-19 20:42:27 +0300

    feat: implement presenter

--- Lab2/MyCalculator/Presenters/CalculatorPresenter.cs
+++ Lab2/MyCalculator/Presenters/CalculatorPresenter.cs
@@ -2,25 +2,78 @@ using MyCalculator.Interfaces;
 
 namespace MyCalculator.Presenters;
 
-public class CalculatorPresenter :ICalculatorPresenter
+public class CalculatorPresenter : ICalculatorPresenter
 {
+    private readonly ICalculator _calculator;
+    private readonly ICalculatorView _calculatorView;
+
+    public CalculatorPresenter(ICalculator calculator, ICalculatorView calculatorView)
+    {
+        _calculator = calculator;
+        _calculatorView = calculatorView;
+    }
+
     public void OnPlusClicked()
     {
-        throw new NotImplementedException();
+        var parseSuccess = TryParseArgumentsAndHandleError(out var first, out var second);
+
+        if (!parseSuccess)
+            return;
+
+        var result = _calculator.Sum(first, second);
+        _calculatorView.PrintResult(result);
     }
 
     public void OnMinusClicked()
     {
-        throw new NotImplementedException();
+        var parseSuccess = TryParseArgumentsAndHandleError(out var first, out var second);
+
+        if (!parseSuccess)
+            return;
+
+        var result = _calculator.Subtract(first, second);
+        _calculatorView.PrintResult(result);
     }
 
     public void OnDivideClicked()
     {
-        throw new NotImplementedException();
+        var parseSuccess = TryParseArgumentsAndHandleError(out var first, out var second);
+
+        if (!parseSuccess)
+            return;
+
+        try
+        {
+            var result = _calculator.Divide(first, second);
+            _calculatorView.PrintResult(result);
+        }
+        catch (DivideByZeroException e)
+        {
+            _calculatorView.DisplayError(e.Message);
+        }
     }
 
     public void OnMultiplyClicked()
     {
-        throw new NotImplementedException();
+        var parseSuccess = TryParseArgumentsAndHandleError(out var first, out var second);
+
+        if (!parseSuccess)
+            return;
+
+        var result = _calculator.Multiply(first, second);
+        _calculatorView.PrintResult(result);
+    }
+
+    private bool TryParseArgumentsAndHandleError(out double first, out double second)
+    {
+        var parseSuccess = double.TryParse(_calculatorView.GetFirstArgumentAsString(), out first);
+        if (!parseSuccess)
+            _calculatorView.DisplayError("Parse first argument error");
+
+        parseSuccess = double.TryParse(_calculatorView.GetSecondArgumentAsString(), out second);
+        if (!parseSuccess)
+            _calculatorView.DisplayError("Parse second argument error");
+
+        return parseSuccess;
     }
 }
