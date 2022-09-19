6f6bfa6728759a763dbdf583e4583331d35891d5
Dmitrii <62088955+Dikin01@users.noreply.github.com>
2022-09-19 15:35:19 +0300

    feat: view implementation

--- Lab2/MyCalculator/Views/CalculatorView.cs
+++ Lab2/MyCalculator/Views/CalculatorView.cs
@@ -4,23 +4,44 @@ namespace MyCalculator.Views;
 
 public class CalculatorView : ICalculatorView
 {
+    private readonly StreamReader _input;
+    private readonly StreamWriter _output;
+    
+    private string? _firstArgument;
+    private string? _secondArgument;
+
+    public CalculatorView(Stream stream)
+    {
+        _input = new StreamReader(stream);
+        _output = new StreamWriter(stream);
+    }
+    
     public void PrintResult(double result)
     {
-        throw new NotImplementedException();
+        _output.WriteLine($"Result: {result}");
+        _output.Flush();
     }
 
     public void DisplayError(string message)
     {
-        throw new NotImplementedException();
+        _output.WriteLine($"Error: {message}");
+        _output.Flush();
     }
 
     public string GetFirstArgumentAsString()
     {
-        throw new NotImplementedException();
+        _firstArgument ??= _input.ReadLine()!;
+        if (_firstArgument is null)
+            throw new InvalidOperationException("Stream doesn't contain first argument");
+        return _firstArgument;
     }
 
     public string GetSecondArgumentAsString()
     {
-        throw new NotImplementedException();
+        GetFirstArgumentAsString();
+        _secondArgument ??= _input.ReadLine()!;
+        if (_secondArgument is null)
+            throw new InvalidOperationException("Stream doesn't contain second argument");
+        return _secondArgument;
     }
 }
