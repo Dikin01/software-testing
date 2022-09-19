e50c52ee9e8ca554139f0d5419998d86326f7866
Dmitrii <62088955+Dikin01@users.noreply.github.com>
2022-09-19 20:41:50 +0300

    refactor: rename workflow and remove useless symbols

--- Lab2/MyCalculator/Models/Calculator.cs
+++ Lab2/MyCalculator/Models/Calculator.cs
@@ -13,9 +13,7 @@ public class Calculator : ICalculator
     public double Divide(double a, double b)
     {
         if (Math.Abs(b) < ICalculator.Epsilon)
-        {
             throw new DivideByZeroException($"|{nameof(b)}| must be greater than {ICalculator.Epsilon:g2}");
-        }
 
         return a / b;
     }
