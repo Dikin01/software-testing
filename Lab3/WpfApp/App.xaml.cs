142203c024b200654065c8a7461a3f5911ab938a
Dmitrii <kondim007@mail.ru>
2022-09-27 13:27:06 +0300

    chore: change folder

---
+++ Lab3/WpfApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MyCalculator.Interfaces;
+using MyCalculator.Models;
+using MyCalculator.Presenters;
+using System.Windows;
+
+namespace WpfApp;
+
+public partial class App : Application
+{
+    private readonly Window _window;
+
+    private readonly ICalculator _calculator;
+    private readonly ICalculatorView _view;
+    private readonly ICalculatorPresenter _presenter;
+
+    public App()
+    {
+        var message = new MessageService();
+        var mainWindow = new MainWindow(message);
+
+        _window = mainWindow;
+
+        _calculator = new Calculator();
+        _view = mainWindow;
+        _presenter = new CalculatorPresenter(_calculator, _view);
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        _window.ShowDialog();
+    }
+}
