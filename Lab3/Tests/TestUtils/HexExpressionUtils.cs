0e8414689d79d34194db7c636867e159be91a99b
Dmitrii <62088955+Dikin01@users.noreply.github.com>
2022-10-24 22:16:22 +0300

    Add UI tests (#11)
    test: UI

---
+++ Lab3/Tests/TestUtils/HexExpressionUtils.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests.TestUtils;
+
+public static class HexExpressionUtils
+{
+    public static byte[] ConvertToBytes(string hexExpression)
+    {
+        var expression = hexExpression.Trim();
+        if (!expression.StartsWith("0x"))
+            throw new FormatException("Expression should start with 0x");
+        expression = new string(expression
+            .Skip(2)
+            .SkipWhile(symbol => symbol == '0')
+            .ToArray());
+
+        var range = Enumerable.Range(0, expression.Length / 2);
+        var result = new List<byte>();
+        foreach (var item in range)
+        {
+            var partExpression = expression.Substring(item * 2, 2);
+            var itemByte = Convert.ToByte(partExpression, 16);
+            result.Add(itemByte);
+        }
+
+        return result.ToArray();
+    }
+
+    public static bool CompareAsHexExpression(string a, string b)
+    {
+        var aBytes = ConvertToBytes(a);
+        var bBytes = ConvertToBytes(b);
+        return aBytes.SequenceEqual(bBytes);
+    }
+}
---
+++ Lab3/WpfApp/MessageWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApp;
+
+public partial class MessageWindow : Window
+{
+    public MessageWindow(string message)
+    {
+        InitializeComponent();
+        TextMessage.Content = message;
+    }
+}
