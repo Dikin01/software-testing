317b8d5543d6a4343b3396f84b76cdb762435f08
Dmitrii <kondim007@mail.ru>
2022-10-25 18:52:27 +0300

    test: add few

--- Lab5/Lab5/SiteTests.cs
+++ Lab5/Lab5/SiteTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace Lab5;
 
@@ -11,22 +12,48 @@ public class SiteTests : IDisposable
     {
         _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
         _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-        _webDriver.Navigate().GoToUrl(@"https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&" + 
-                                      @"st.cmd=anonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone");
         _webDriver.Manage().Window.Maximize();
     }
-    
+
     [Fact]
-    public void Registration_ShouldBeFailed_WhenPhoneNumberIsNotCorrected()
+    public void Registration_ShouldShowFailed_WhenPhoneNumberIsNotCorrected()
     {
+        _webDriver.Navigate().GoToUrl(@"https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&" +
+                                      @"st.cmd=anonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone");
         var phoneInput = _webDriver.FindElement(By.Name("st.r.phone"));
-        phoneInput.SendKeys("910835174");
+        phoneInput.SendKeys("qwerty");
         var registrationButton = _webDriver.FindElement(By.XPath("/html/body"));
-        
+
         registrationButton.Click();
 
         var message = _webDriver.FindElement(By.ClassName("input-e"));
         message.Text.Should().Be("Неправильный номер телефона.");
+    }
+
+    [Fact]
+    public void FindVideo_ShouldShowEmptyList_WhenRequestIsNotCorrected()
+    {
+        _webDriver.Navigate().GoToUrl("https://vk.com/video");
+        var searchInput = _webDriver.FindElement(By.Id("video_search_input"));
+        searchInput.SendKeys("/");
+
+        var result = _webDriver.FindElement(By.ClassName("VideoEmptyStub__text"));
+
+        Thread.Sleep(500);
+        result.Text.Should().Be("По запросу /\r\nне найдено ни одного видео");
+    }
+
+    [Fact]
+    public void SelectGroup_ShouldShowNoEmptySchedule_WhenGroupNameIsCorrected()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.miet.ru/schedule");
+        var groups = _webDriver.FindElement(By.ClassName("group"));
+        var selector = new SelectElement(groups);
+
+        selector.SelectByValue("ПИН-44");
+
+        var result = _webDriver.FindElement(By.ClassName("data"));
+        result.Text.Should().NotBeEmpty();
     }
 
     public void Dispose()
